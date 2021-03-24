6a7c8c548191833b5eb3cbe822345e9eb78534b1
Aqua <aqua0210@163.com>
2021-03-24 23:51:53 +0800

    finished implement protocol.

--- src/protocol.nim
+++ src/protocol.nim
@@ -1,7 +1,22 @@
+import json
+
 type
   Message* = object
     username* : string
     message* : string
 
 proc parseMessage* (data: string): Message =
-  discard
+  let dataJson = parseJson(data)
+  result.username = dataJson["username"].getStr()
+  result.message = dataJson["message"].getStr()
+
+
+
+when isMainModule:
+  block:
+    let data = """{"username": "John", "message": "Hi!"}"""
+    let parsed = parseMessage(data)
+    doAssert parsed.username == "John"
+    doAssert parsed.message == "Hi!"
+    echo("All tests passed!")
+
